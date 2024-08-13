7e02f167255eb93856a489a887e3117ea8d4c79b
Gregg Code <118491204+jGreggCode@users.noreply.github.com>
2024-08-13 13:42:35 +0800

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
+const PORT = process.env.PORT || 8080;
 http.createServer(function (req, res) {
   res.write("I'm Alive");
-  res.end();                
-}).listen(8080);
+  res.end();
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
